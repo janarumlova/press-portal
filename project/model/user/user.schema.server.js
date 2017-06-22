88d8f3f0f3f3a7cbd50d8bb5eeeef5b8ddcc78df
Sidharth Thapar <sidharththapar@yahoo.co.in>
2017-06-21 21:42:57 -0400

    first commit

---
+++ project/model/user/user.schema.server.js
@@ -0,0 +1,26 @@
+/**
+ * Created by sidharththapar on 6/11/17.
+ */
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    username: {type: String, unique: true},
+    password: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    roles:[{type: String,
+        enum: ['READER', 'ADMIN', 'PUBLISHER']}],
+    google: {
+        id:    String,
+        token: String
+    },
+    articles: [{type:mongoose.Schema.Types.ObjectId, ref: "PostModel"}],
+    subscriptions: [{type:mongoose.Schema.Types.ObjectId, ref: "UserModel"}],
+    followers: [{type:mongoose.Schema.Types.ObjectId, ref: "UserModel"}],
+    followedBy: [{type:mongoose.Schema.Types.ObjectId, ref: "UserModel"}],
+    comments: [{type:mongoose.Schema.Types.ObjectId, ref: "CommentModel"}],
+    dateCreated: {type: Date, default: Date.now}
+}, {collection: "user"});
+
+module.exports = userSchema;
